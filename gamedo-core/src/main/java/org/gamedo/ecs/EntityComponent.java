2441cafdb1a3f02eb1aa416d17aaf390a8e4992f
pcloves <pcloves@sina.com>
2021-11-15 17:57:02 +0800

    - 增加IEntity和IComponent的接口注释 - 修复IDEA警告 - IComponent增加setOwner接口函数 - Component增加无参构造函数

--- gamedo-core/src/main/java/org/gamedo/ecs/EntityComponent.java
+++ gamedo-core/src/main/java/org/gamedo/ecs/EntityComponent.java
@@ -2,10 +2,12 @@ package org.gamedo.ecs;
 
 import org.gamedo.ecs.interfaces.IEntity;
 
+@SuppressWarnings("unused")
 public class EntityComponent extends Component<IEntity> {
-
     public EntityComponent(IEntity owner) {
         super(owner);
     }
 
+    public EntityComponent() {
+    }
 }
